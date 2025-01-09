ad9da03e2aeef807c6f6ae68f59ebae0dfef13b3
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2025-01-09 17:09:56 +0530

    Perfome some modifications

--- frontend/src/components/ContactUs.jsx
+++ frontend/src/components/ContactUs.jsx
@@ -11,17 +11,28 @@ const ContactUs = () => {
                     <h2 class="text-gray-800 text-base font-bold">Email</h2>
                     <ul class="mt-4">
                         <li class="flex items-center">
-                            <div class="bg-[#e6e6e6cf] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill='#007bff'
-                                    viewBox="0 0 479.058 479.058">
-                                    <path
-                                        d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"
-                                        data-original="#000000" />
-                                </svg>
-                            </div>
                             <a href="javascript:void(0)" class="text-[#007bff] text-sm ml-4">
-                                <small class="block">Mail</small>
                                 <strong>services.codexintern@gmail.com</strong>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="mt-5">
+                    <h2 class="text-gray-800 text-base font-bold">Contact</h2>
+                    <ul class="mt-4">
+                        <li class="flex items-center">
+                            <a href="javascript:void(0)" class="text-[#007bff] text-sm ml-4">
+                                <strong>+91 8337057568</strong>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="mt-5">
+                    <h2 class="text-gray-800 text-base font-bold">Address</h2>
+                    <ul class="mt-4">
+                        <li class="flex items-center">
+                            <a href="javascript:void(0)" class="text-[#007bff] text-sm ml-4">
+                                <strong>46A, Sarat Bose Colony, Kolkata - 700078, PO: Haltu, West Bengal, India.</strong>
                             </a>
                         </li>
                     </ul>
