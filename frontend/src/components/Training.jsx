6bd84bdd308f7e68abc1b882a7b717619bc82c2d
rahul-ghosh-2004 <iamrahulghoshinfo@gmail.com>
2025-03-03 01:56:55 +0530

    Course updation

--- frontend/src/components/Training.jsx
+++ frontend/src/components/Training.jsx
@@ -10,12 +10,41 @@ const Training = () => {
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-sm:gap-8">
 
           <div class="bg-white rounded overflow-hidden px-1 py-1 shadow-lg">
-            <img src="https://tolustar.com/wp-content/uploads/2020/02/Front-end-Development.jpeg" alt="Blog Post 1" class="w-full h-52 object-cover" />
+            <img src="https://www.mygreatlearning.com/blog/wp-content/uploads/2019/09/What-is-data-science-2.jpg" alt="Blog Post 1" class="w-full h-52 object-cover" />
             <div class="p-6">
-              <h3 class="text-lg font-bold text-gray-800 mb-3">Basic Web Development</h3>
-              <p class="text-gray-500 text-sm">Want to deep dive in the field of Web Dev. Join our training program to master HTML, CSS & JavaScript</p>
-              <p class="text-gray-800 text-[13px] font-semibold mt-4">10 March, 2025</p>
-              <a href="https://rzp.io/rzp/Gp4FkO9" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-purple-600 hover:bg-purple-700 text-white text-[13px]">119/- Only</a>
+              <h3 class="text-lg font-bold text-gray-800 mb-3">Data Science with Python</h3>
+              <p class="text-gray-500 text-sm">Want to deep dive into the field of Data Science? Join our training program to master Python, Data Analysis, and Machine Learning.</p>
+              <div className='flex items-center justify-between gap-x-5'>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">17th March, 2025</p>
+                <a href='#' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
+              </div>
+              <a href="https://rzp.io/rzp/uWbCPazP" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-red-500 hover:bg-red-700 text-white text-[13px]">199/- Only</a>
+            </div>
+          </div>
+
+          <div class="bg-white rounded overflow-hidden px-1 py-1 shadow-lg">
+            <img src="https://images.prismic.io/loco-blogs/79328284-f97b-489f-924c-eb3b17e34b56_image2.png?auto=compress%2Cformat&rect=0%2C0%2C1999%2C1124&w=1920&h=1080&ar=1.91%3A1" alt="Blog Post 3" class="w-full h-52 object-cover" />
+            <div class="p-6">
+              <h3 class="text-lg font-bold text-gray-800 mb-3">Mern Stack Development</h3>
+              <p class="text-gray-500 text-sm">Industry standard MERN Stack training using React, Express, Node, Mongodb. Training program has not been opened yet. Stay tuned ...</p>
+              <div className='flex items-center justify-between gap-x-5'>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">17th March, 2025</p>
+                <a href='#' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
+              </div>
+              <a href="https://rzp.io/rzp/MQT8dnR" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-red-500 hover:bg-red-700 text-white text-[13px]">199/- Only</a>
+            </div>
+          </div>
+
+          <div class="bg-white rounded overflow-hidden px-1 py-1 shadow-lg">
+            <img src="https://www.exasol.com/app/uploads/2024/10/thumbnail-blog-what-is-a-relational-database.webp" alt="Blog Post 3" class="w-full h-52 object-cover" />
+            <div class="p-6">
+              <h3 class="text-lg font-bold text-gray-800 mb-3">MySQL/OracleDB & MongoDB</h3>
+              <p class="text-gray-500 text-sm">Want to deep dive into the field of Database Management? Join our training program to master MySQL, OracleSQL, and MongoDB.</p>
+              <div className='flex items-center justify-between gap-x-5'>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">17th March, 2025</p>
+                <a href='#' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
+              </div>
+              <a href="https://rzp.io/rzp/p2INHIj" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-green-600 hover:bg-green-700 text-white text-[13px]">149/- Only</a>
             </div>
           </div>
 
@@ -24,18 +53,11 @@ const Training = () => {
             <div class="p-6">
               <h3 class="text-lg font-bold text-gray-800 mb-3">Front-End Development</h3>
               <p class="text-gray-500 text-sm">Go deeper & Master Front-End Web Development using React, Tailwind, Shadcn & Radix UI. Beginner to Advance.</p>
-              <p class="text-gray-800 text-[13px] font-semibold mt-4">10 March, 2025</p>
+              <div className='flex items-center justify-between gap-x-5'>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">17th March, 2025</p>
+                <a href='#' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
+              </div>
               <a href="https://rzp.io/rzp/I8XO0O3V" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-purple-600 hover:bg-purple-700 text-white text-[13px]">119/- Only</a>
-            </div>
-          </div>
-
-          <div class="bg-white rounded overflow-hidden px-1 py-1 shadow-lg">
-            <img src="https://www.uidm.in/wp-content/uploads/2023/08/curso-python-gratis-facialix.jpg" alt="Blog Post 3" class="w-full h-52 object-cover" />
-            <div class="p-6">
-              <h3 class="text-lg font-bold text-gray-800 mb-3">Python Development</h3>
-              <p class="text-gray-500 text-sm">Master Python Programming for development. Learn different libraries in Python. Basic to Advance Training Program.</p>
-              <p class="text-gray-800 text-[13px] font-semibold mt-4">10 March, 2025</p>
-              <a href="https://rzp.io/rzp/9Ntc3YNs" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-purple-600 hover:bg-purple-700 text-white text-[13px]">119/- Only</a>
             </div>
           </div>
 
@@ -44,18 +66,11 @@ const Training = () => {
             <div class="p-6">
               <h3 class="text-lg font-bold text-gray-800 mb-3">Back-End Development</h3>
               <p class="text-gray-500 text-sm">Insustry standard backend using NodeJS, ExpressJS, MongoDB, Mongoose ODM, JsonWebToken, Multer, Cloudinary.</p>
-              <p class="text-gray-800 text-[13px] font-semibold mt-4">10 March, 2025</p>
+              <div className='flex items-center justify-between gap-x-5'>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">17th March, 2025</p>
+                <a href='#' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
+              </div>
               <a href="https://rzp.io/rzp/FW9AANrp" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-purple-600 hover:bg-purple-700 text-white text-[13px]">119/- Only</a>
-            </div>
-          </div>
-
-          <div class="bg-white rounded overflow-hidden px-1 py-1 shadow-lg">
-            <img src="https://codequality.us/wp-content/uploads/2023/11/KB-MERN-Featured.jpg" alt="Blog Post 3" class="w-full h-52 object-cover" />
-            <div class="p-6">
-              <h3 class="text-lg font-bold text-gray-800 mb-3">Mern Stack Development</h3>
-              <p class="text-gray-500 text-sm">Industry standard MERN Stack training using React, Express, Node, Mongodb. Training program has not been opened yet. Stay tuned ...</p>
-              <p class="text-gray-800 text-[13px] font-semibold mt-4">Comming Soon ...</p>
-              <a href="javascript:void(0);" class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-red-500 hover:bg-red-700 text-white text-[13px]">Comming Soon</a>
             </div>
           </div>
 
@@ -64,7 +79,10 @@ const Training = () => {
             <div class="p-6">
               <h3 class="text-lg font-bold text-gray-800 mb-3">Full Stack App Development using React Native</h3>
               <p class="text-gray-500 text-sm">Full Stack Cross Platform App Development using React-Native.</p>
-              <p class="text-gray-800 text-[13px] font-semibold mt-4">Comming Soon ...</p>
+              <div className='flex items-center justify-between gap-x-5'>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">Comming Soon ...</p>
+                <a href='#' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
+              </div>
               <a href="javascript:void(0);" class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-red-500 hover:bg-red-700 text-white text-[13px]">Comming Soon</a>
             </div>
           </div>
