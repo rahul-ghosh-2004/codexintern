9715f4e7e366df1ff6d18c2f2f565a8b4f171213
rahul-ghosh-2004 <iamrahulghoshinfo@gmail.com>
2025-07-17 01:44:26 +0530

    TEMP

--- frontend/src/components/Training.jsx
+++ frontend/src/components/Training.jsx
@@ -13,12 +13,13 @@ const Training = () => {
             <img src="https://www.mygreatlearning.com/blog/wp-content/uploads/2019/09/What-is-data-science-2.jpg" alt="Blog Post 1" class="w-full h-52 object-cover" />
             <div class="p-6">
               <h3 class="text-lg font-bold text-gray-800 mb-3">Data Science with Python</h3>
-              <p class="text-gray-500 text-sm">Want to deep dive into the field of Data Science? Join our training program to master Python, Data Analysis, and Machine Learning.</p>
+              <p class="text-gray-500 text-sm">Turn raw data into powerful insights and predictions — from scratch!
+              </p>
               <div className='flex items-center justify-between gap-x-5'>
-                <p class="text-gray-800 text-[13px] font-semibold mt-4">15th May, 2025</p>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">10th August, 2025</p>
                 <a href='https://drive.google.com/file/d/1DF-Zgj4ToLf_Z3RgcYe-Qehyd_LXJ1_a/view?usp=sharing' target='_blank' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
               </div>
-              <a href="https://rzp.io/rzp/uWbCPazP" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-red-500 hover:bg-red-700 text-white text-[13px]">199/- Only</a>
+              <a href="https://rzp.io/rzp/uWbCPazP" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-red-500 hover:bg-red-700 text-white text-[13px]">449/- Only</a>
             </div>
           </div>
 
@@ -26,25 +27,27 @@ const Training = () => {
             <img src="https://images.prismic.io/loco-blogs/79328284-f97b-489f-924c-eb3b17e34b56_image2.png?auto=compress%2Cformat&rect=0%2C0%2C1999%2C1124&w=1920&h=1080&ar=1.91%3A1" alt="Blog Post 3" class="w-full h-52 object-cover" />
             <div class="p-6">
               <h3 class="text-lg font-bold text-gray-800 mb-3">Mern Stack Development</h3>
-              <p class="text-gray-500 text-sm">Industry standard MERN Stack training using React, Express, Node, Mongodb. Training program has not been opened yet. Stay tuned ...</p>
+              <p class="text-gray-500 text-sm">Become a full-stack developer and build powerful, modern web applications from scratch!
+              </p>
               <div className='flex items-center justify-between gap-x-5'>
-                <p class="text-gray-800 text-[13px] font-semibold mt-4">15th May, 2025</p>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">10th August, 2025</p>
                 <a href='https://drive.google.com/file/d/1_GPjtk852gBJf0NwUYiMqG77u-0k0_vM/view?usp=sharing' target='_blank' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
               </div>
-              <a href="https://rzp.io/rzp/MQT8dnR" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-red-500 hover:bg-red-700 text-white text-[13px]">199/- Only</a>
+              <a href="https://rzp.io/rzp/MQT8dnR" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-red-500 hover:bg-red-700 text-white text-[13px]">449/- Only</a>
             </div>
           </div>
 
           <div class="bg-white rounded overflow-hidden px-1 py-1 shadow-lg">
-            <img src="https://www.exasol.com/app/uploads/2024/10/thumbnail-blog-what-is-a-relational-database.webp" alt="Blog Post 3" class="w-full h-52 object-cover" />
+            <img src="https://dypatil-edu-cms.s3.ap-south-1.amazonaws.com/wp-content/uploads/2023/07/05104810/computer-science-ai-machine-learning.jpg" alt="Blog Post 3" class="w-full h-52 object-cover" />
             <div class="p-6">
-              <h3 class="text-lg font-bold text-gray-800 mb-3">MySQL/OracleDB & MongoDB</h3>
-              <p class="text-gray-500 text-sm">Want to deep dive into the field of Database Management? Join our training program to master MySQL, OracleSQL, and MongoDB.</p>
+              <h3 class="text-lg font-bold text-gray-800 mb-3">AI & ML</h3>
+              <p class="text-gray-500 text-sm">Explore the world of Artificial Intelligence and Machine Learning — learn to build smart, data-driven applications!
+              </p>
               <div className='flex items-center justify-between gap-x-5'>
-                <p class="text-gray-800 text-[13px] font-semibold mt-4">15th May, 2025</p>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">10th August, 2025</p>
                 <a href='#' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
               </div>
-              <a href="https://rzp.io/rzp/p2INHIj" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-green-600 hover:bg-green-700 text-white text-[13px]">149/- Only</a>
+              <a href="https://rzp.io/rzp/p2INHIj" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-green-600 hover:bg-green-700 text-white text-[13px]">449/- Only</a>
             </div>
           </div>
 
@@ -52,12 +55,13 @@ const Training = () => {
             <img src="https://www.creativeitinstitute.com/images/course/course_1674371266.jpg" alt="Blog Post 2" class="w-full h-52 object-cover" />
             <div class="p-6">
               <h3 class="text-lg font-bold text-gray-800 mb-3">Front-End Development</h3>
-              <p class="text-gray-500 text-sm">Go deeper & Master Front-End Web Development using React, Tailwind, Shadcn & Radix UI. Beginner to Advance.</p>
+              <p class="text-gray-500 text-sm">Build modern, interactive, and production-ready web applications — from scratch!
+              </p>
               <div className='flex items-center justify-between gap-x-5'>
-                <p class="text-gray-800 text-[13px] font-semibold mt-4">15th May, 2025</p>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">10th August, 2025</p>
                 <a href='https://drive.google.com/file/d/1Rd2p2WN1bpBV_zvdWgn61KG36CrU4fbS/view?usp=sharing' target='_blank' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
               </div>
-              <a href="https://rzp.io/rzp/I8XO0O3V" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-purple-600 hover:bg-purple-700 text-white text-[13px]">199/- Only</a>
+              <a href="https://rzp.io/rzp/FW9AANrp" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-purple-600 hover:bg-purple-700 text-white text-[13px]">329/- Only</a>
             </div>
           </div>
 
@@ -65,12 +69,13 @@ const Training = () => {
             <img src="https://t4.ftcdn.net/jpg/02/99/62/11/360_F_299621124_vutDKbNwRJG6poJRQQIMYfsc4tJCTO5E.jpg" alt="Blog Post 3" class="w-full h-52 object-cover" />
             <div class="p-6">
               <h3 class="text-lg font-bold text-gray-800 mb-3">Back-End Development</h3>
-              <p class="text-gray-500 text-sm">Insustry standard backend using NodeJS, ExpressJS, MongoDB, Mongoose ODM, JsonWebToken, Multer, Cloudinary.</p>
+              <p class="text-gray-500 text-sm">Learn how to build powerful, secure, and scalable backend applications — from scratch!
+              </p>
               <div className='flex items-center justify-between gap-x-5'>
-                <p class="text-gray-800 text-[13px] font-semibold mt-4">15th May, 2025</p>
+                <p class="text-gray-800 text-[13px] font-semibold mt-4">10th August, 2025</p>
                 <a href='https://drive.google.com/file/d/1pbFZA5-VEZGcy4Dv17kDtE57bg5ZIkeP/view?usp=sharing' target='_blank' className='mt-4 text-sm text-blue-900 underline'>Brochure</a>
               </div>
-              <a href="https://rzp.io/rzp/FW9AANrp" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-purple-600 hover:bg-purple-700 text-white text-[13px]">199/- Only</a>
+              <a href="https://rzp.io/rzp/I8XO0O3V" target='_blank' class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-purple-600 hover:bg-purple-700 text-white text-[13px]">329/- Only</a>
             </div>
           </div>
 
